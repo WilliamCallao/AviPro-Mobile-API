b4be724e10ca2045b74edea6b1acfb6f18876e73
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-06-16 13:29:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,25 +11,41 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-
 app.use(clienteRoutes);
 app.use(notasPendientesRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Avi Pro!')
+  res.send('alive, alive!');
 });
 
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-    return sequelize.sync();
-  })
-  .then(() => {
-    console.log('Database synchronized.');
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+const startServer = async () => {
+  let retries = 5;
+  while (retries) {
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+      await sequelize.sync();
+      console.log('Database synchronized.');
+      app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+        console.log(`         ) (`);
+        console.log(`        (   ) )`);
+        console.log(`         ) ( (`);
+        console.log(`       _______)_`);
+        console.log(`    .-'---------|`);
+        console.log(`   ( c|         |`);
+        console.log(`    '-.         |`);
+        console.log(`      '_________'`);
+        console.log(`       '-------'`);
+      });
+      break;
+    } catch (err) {
+      console.error('Unable to connect to the database:', err);
+      retries -= 1;
+      console.log(`Retries left: ${retries}`);
+      await new Promise(res => setTimeout(res, 5000));
+    }
+  }
+};
+
+startServer();
